d56a365093351ace0acaabce4a571d938e52d130
Luisa Carrillo <Luvluvxc@users.noreply.github.com>
2026-05-06 14:58:18 -0600

    useMemo//Propinas

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,26 +1,25 @@
 import { formatCurrency } from "../helpers";
 import type { OrderItem } from "../types";
 import { useMemo } from "react";
-import type { Dispatch, SetStateAction } from 'react';
-
-
-
-
+import type { Dispatch, SetStateAction } from "react";
 
 type OrderTotalProps = {
-	order: OrderItem[]
-	tip: number                            
-  setTip: Dispatch<SetStateAction<number>>
-}
-
+  order: OrderItem[];
+  tip: number;
+  setTip: Dispatch<SetStateAction<number>>;
+};
 
 export const OrderTotals = ({ order, tip, setTip }: OrderTotalProps) => {
-	
-	const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.price * item.quantity), 0), [order])
-	const tipAmount = subtotalAmount * tip        // 🔢 calcula la propina
-  const totalAmount = subtotalAmount + tipAmount // 🔢 calcula el total
+  //reduce sirve para recorrer los elementos de las cosos que trigo
+  const subtotalAmount = useMemo(
+    () => order.reduce((total, item) => total + item.price * item.quantity, 0),
+    [order],
+  );
 
-
+  const tipAmount = useMemo(() => subtotalAmount * tip, [subtotalAmount, tip]); // calcula la propina UseMemo siempre leva callback
+  const totalAmount = useMemo(() => subtotalAmount + tipAmount,
+    [subtotalAmount, tipAmount],
+  );
 
   return (
     <>
@@ -30,31 +29,34 @@ export const OrderTotals = ({ order, tip, setTip }: OrderTotalProps) => {
         {/* Subtotal */}
         <div className="flex justify-between text-sm">
           <span className="text-gray-500">Subtotal</span>
-					<span className="font-medium">{ formatCurrency(subtotalAmount )}</span>
+          <span className="font-medium">{formatCurrency(subtotalAmount)}</span>
         </div>
 
         {/* Propina */}
         <div className="flex justify-between text-sm">
           <span className="text-gray-500">Propina</span>
-					<span className="font-medium"></span>
-					<span>{formatCurrency(tipAmount)}</span>
+          <span className="font-medium"></span>
+          <span>{formatCurrency(tipAmount)}</span>
         </div>
 
         {/* Botones de porcentaje */}
         <div className="grid grid-cols-3 gap-2">
-					<button
-						onClick={() => setTip(0.25)}
-						className="py-2 rounded border text-sm font-medium hover:bg-gray-100" >
+          <button
+            onClick={() => setTip(0.25)}
+            className="py-2 rounded border text-sm font-medium hover:bg-gray-100"
+          >
             25%
           </button>
-					<button
-						onClick={() => setTip(0.50)}
-						className="py-2 rounded border text-sm font-medium hover:bg-gray-100">
+          <button
+            onClick={() => setTip(0.5)}
+            className="py-2 rounded border text-sm font-medium hover:bg-gray-100"
+          >
             50%
           </button>
-					<button
-						onClick={() => setTip(0.70)}
-						className="py-2 rounded border text-sm font-medium hover:bg-gray-100">
+          <button
+            onClick={() => setTip(0.7)}
+            className="py-2 rounded border text-sm font-medium hover:bg-gray-100"
+          >
             70%
           </button>
         </div>
@@ -63,10 +65,16 @@ export const OrderTotals = ({ order, tip, setTip }: OrderTotalProps) => {
 
         {/* Total */}
         <div className="flex justify-between text-base font-semibold">
-					<span>Total</span>
-					
-					<span>{formatCurrency(totalAmount) /* Format Currency es para que agarre el tipo de moneda ya establecida :)*/}</span>
-				</div>
+          <span>Total</span>
+
+          <span>
+            {
+              formatCurrency(
+                totalAmount,
+              ) /* Format Currency es para que agarre el tipo de moneda ya establecida :)*/
+            }
+          </span>
+        </div>
       </div>
     </>
   );
